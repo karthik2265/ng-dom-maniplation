a8f60d04f1b0df1cc4dcc0988297d6a89b508deb
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2022-10-30 10:47:35 +0530

    templateView

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 #superhero1>I'am Batman</h1>
+    <ng-container #superheroes></ng-container>
+    <button (click)="addSuperHero()">Add Superhero</button>
+    <ng-template #template>
+      <h1>Superman</h1>
+    </ng-template>
+  `,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'dom-manipulation';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('superhero1') superhero1!: ElementRef;
+  @ViewChild('template') myTemplate!: TemplateRef<any>;
+  @ViewChild('superheroes', { read: ViewContainerRef })
+  superheroes!: ViewContainerRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.superhero1.nativeElement.textContent); // outputs: I'am Batman
+  }
+
+  addSuperHero() {
+    const templateView = this.myTemplate.createEmbeddedView(null);
+    this.superheroes.insert(templateView);
+  }
 }
